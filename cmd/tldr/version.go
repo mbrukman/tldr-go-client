36adca9c5777bf9420cead9cbabdef3617d22a56
Tom Bell <tomb@tomb.io>
2018-03-22 22:12:00 +0000

    Add NOTE about compile-time variables

--- cmd/tldr/version.go
+++ cmd/tldr/version.go
@@ -1,5 +1,8 @@
 package main
 
+// The values of these variables are injected at compile time from the VERSION
+// environment variable and latest Git commit.
+
 // Version is the current version of tldr.
 var Version = "dev"
 
