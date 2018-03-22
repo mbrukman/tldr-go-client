ae6a2aa52c8b4375e8e0a76fb51a2abf4e21d941
Tom Bell <tomb@tomb.io>
2018-03-22 22:12:15 +0000

    Add initial flag/arg parsing

--- cmd/tldr/main.go
+++ cmd/tldr/main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const helpText = `usage: tldr [command]
+  -version
+      Display version
+`
+
+var (
+	version = flag.Bool("version", false, "")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, helpText)
+	os.Exit(2)
+}
+
 func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if *version {
+		fmt.Fprintf(os.Stdout, "tldr %s (%s)\n", Version, Commit)
+		os.Exit(0)
+	}
+
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.Usage()
+	}
 }
