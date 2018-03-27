70e3baa6746e11d75bd43b614d9150275ecc8d2d
Tom Bell <tomb@tomb.io>
2018-03-27 22:40:32 +0100

    Add simple implementation of rendering page

--- tldr.go
+++ tldr.go
@@ -1,6 +1,18 @@
 package tldr
 
+import (
+	"fmt"
+	"os"
+)
+
 // Show render the TLDR page for the given command.
 func Show(cmd string) error {
+	bytes, ok := Assets[fmt.Sprintf("%s.md", cmd)]
+	if !ok {
+		return fmt.Errorf("could not find page for %s", cmd)
+	}
+
+	fmt.Fprintf(os.Stdout, string(bytes))
+
 	return nil
 }
